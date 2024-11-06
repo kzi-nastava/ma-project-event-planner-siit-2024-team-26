b433861821167e7489f20613cf99e5b0f712151c
Luka-Vukovic <lukav400@gmail.com>
2024-11-06 23:53:08 +0100

    [ADD] First version of splash screen and log in

---
+++ App/app/src/main/java/com/example/eventplanner/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.eventplanner;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.splashscreen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        doTransition();
+    }
+
+    private void doTransition() {
+        Timer transitionTimer = new Timer();
+        TimerTask transitionTask = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        transitionTimer.schedule(transitionTask, 5000);
+    }
+}
